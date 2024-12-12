a9ae5c962d7a9bcbef84c3584291d42812ab9c72
YasiruIndunil <yasiru.indunil1@gmail.com>
2024-12-12 11:16:33 +0530

    Initial Comit

---
+++ TheGalleryCafe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheGalleryCafe.Startup))]
+namespace TheGalleryCafe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
